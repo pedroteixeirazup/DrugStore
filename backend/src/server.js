eb3a4d05ad2104e08a6b47c5897ec0f2248e3273
Pedro <pedroteixeira@zup.com>
2019-08-23 11:05:59 -0300

    Modified server to local ethernet adress.

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3333;
 
 require('dotenv/config')
 
-console.log(process.env.PORT);
-console.log(process.env.PASSWORD)
+// console.log(process.env.PORT);
+// console.log(process.env.PASSWORD)
 const app = express();
 
 mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@omnistack-vlfdq.mongodb.net/test?retryWrites=true&w=majority`, {
@@ -20,4 +20,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => console.log(`App running on port ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`App running on port ${port}`));
